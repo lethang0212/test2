f059ac3d953df41fbb92a272d2d731f59ddf8eca
thangld <thangld@vnext.com.vn>
2021-07-05 16:24:23 +0700

    example-redux-basic

---
+++ src/store/action.js
@@ -0,0 +1,6 @@
+export const changeColor = (color) => {
+    return {
+        type: 'GET_COLOR',
+        color,
+    }
+}
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+const STATE = {
+    color: 'green',
+}
+
+export default function reducer(state = STATE, action) {
+    switch(action.type) {
+        case 'GET_COLOR' :
+            return {...state, color: action.color};
+        default : 
+            return state;
+    }
+}
